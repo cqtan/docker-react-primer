32ee01b90f5161222aa3789d415093daebd4a566
ctan <s0547139@htw-berlin.de>
2019-11-11 22:15:26 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
   return (
@@ -16,11 +16,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Using Nginx?
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
